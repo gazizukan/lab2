12bd597fdbd4b92cd173481ae55569b5c011de0c
gazizukan <gaziz.nis@gmail.com>
2016-03-03 12:50:59 +0600

    commit

---
+++ ConsoleApplication3/ConsoleApplication3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Student
+    {
+        public string name;
+        public string surname;
+        public Double GPA;
+
+        public Student(string _name, string _surname, Double _gpa)
+        {
+            this.name = _name;
+            this.surname = _surname;
+            this.GPA = _gpa;
+        }
+
+        public override string ToString()
+        {
+            return name + " " + surname + " " + GPA;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            string surname = Console.ReadLine();
+            Double gpa = Convert.ToDouble(Console.ReadLine());
+            Student information = new Student(name, surname, gpa);
+
+            Console.WriteLine(information);
+            Console.ReadKey();
+        }
+    }
+}
