507653840234a59bb48ae1f51ad7692717e0fa25
gazizukan <gaziz.nis@gmail.com>
2016-03-03 12:46:42 +0600

    commit

---
+++ ConsoleApplication4/ConsoleApplication4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using ConsoleApplication4;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student obj = new Student(" Gaziz", "123DA78JD", 3.44);
+            Console.WriteLine(obj.GetID());
+            obj.SetGPA(3.56);
+            Student obj2 = new Student("Alibi", "123DA78J3", 3.89);
+            if (obj > obj2)
+            {
+                Console.WriteLine("Perviy student");
+            }
+            else
+            {
+                Console.WriteLine("Vtoroy student");
+            }
+            //Object
+            Student c = obj + obj2;
+            //c.
+            Console.WriteLine(c);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ ConsoleApplication4/ConsoleApplication4/Student.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using ConsoleApplication4;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Student
+    {
+        public string name;
+        public string id;
+        public double gpa;
+        
+
+        public Student()
+        {
+            this.name = "Unknown";
+            this.id = "unknown";
+            this.gpa = 0;
+        }
+
+        public Student(string name, string id, double gpa)
+        {
+            this.name = name;
+            this.id = id;
+            this.gpa = gpa;
+        }
+
+        public Student(string name, string id)
+        {
+            this.name = name;
+            this.id = id;
+            this.gpa = 0;
+        }
+
+        public string GetID()
+        {
+            return this.id;
+        }
+
+        //GETTER FOR GPA
+        //GETTER FOR NAME
+
+        public void SetGPA(double gpa)
+        {
+
+            this.gpa = gpa;
+        }
+
+        //SETTER FOR NAME
+        //SETTER FOR ID
+
+        public static bool operator >(Student a, Student b)
+        {
+            return a.gpa > b.gpa;
+        }
+
+        public static bool operator <(Student a, Student b)
+        {
+            return a.gpa < b.gpa;
+        }
+
+        public static Student operator +(Student a, Student b)
+        {
+            return new Student(a.name + " " + b.name, a.id + " " + b.id, (a.gpa + b.gpa) / 2);
+        }
+
+        public override string ToString()
+        {
+            return " Меня зовут " + this.name + ", мой GPA " + this.gpa;
+        }
+        
+        
+
+    }
+}
